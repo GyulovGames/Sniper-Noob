af3ddef1ce0972b6bddcefc09ea76ae814f2f712
GyulovGames <155297083+GyulovGames@users.noreply.github.com>
2024-02-21 22:10:00 +0300

    Игра готова!
    - вылечил все болячки игры
    - провел полное тестирование
    - делаю промо материалы

--- Assets/Game/Scripts/Togle.cs
+++ Assets/Game/Scripts/Togle.cs
@@ -3,16 +3,13 @@ using UnityEngine.Events;
 
 public class Togle : MonoBehaviour
 {
+    [SerializeField] private BoxCollider2D boxcollider2D;
+
     public UnityEvent action;
 
     private bool isOpen = false;
 
     private void OnCollisionEnter2D(Collision2D collision)
-    {
-        ToggleSwitch();
-    }
-
-    public void ToggleSwitch()
     {
         action.Invoke();
         Transform childTransform = transform.GetChild(0).transform;
@@ -20,11 +17,14 @@ public class Togle : MonoBehaviour
         if (!isOpen)
         {
             childTransform.localEulerAngles = new Vector3(0, 0, 30);
+            boxcollider2D.offset = new Vector2(-0.21f, 0.27f);
             isOpen = true;
         }
         else if (isOpen)
         {
             childTransform.localEulerAngles = new Vector3(0, 0, -30);
+            boxcollider2D.offset = new Vector2(0.21f, 0.27f);
+
             isOpen = false;
         }
     }
